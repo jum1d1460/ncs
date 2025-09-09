c7c132ac6f6570ba0e2356fef9a27c9785ddc284
Juan Miguel Diago <jum1d1460@gmail.com>
2025-09-09 16:23:04 +0200

    Fix build to view correctly as static web

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,6 +4,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
+    // Forzar rutas relativas para soportar abrir con file://
+    base: './',
+    build: {
+        assetsPrefix: './'
+    },
    vite: {
         server: {
             fs: {
